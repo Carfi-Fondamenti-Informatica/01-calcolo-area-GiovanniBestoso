b5c9eaa014b4fb9d42cceaf418fffa8a0ad71d21
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-11-09 10:49:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+ float a=0, b=0, c=0;
+ float area_triangolo=0, area_quadrato=0, area_rettangolo=0, area_trapezio=0;
+ cin >> a >> b >> c;
+ area_triangolo= (a*b)/2;
+ area_quadrato= a*a;
+ area_rettangolo= a*b;
+ area_trapezio= ((a+b)*c)/2;
+ cout<< "area triangolo: "<< area_triangolo <<endl;
+ cout<< "area quadrato: "<< area_quadrato<<endl;
+ cout<< "area rettangolo: "<< area_rettangolo<<endl;
+ cout<< " area trapezio: "<< area_trapezio<<endl;
+  return 0;
 }
